4730ca03d9d19cb192b78fd8035f73f4ba45a7cb
Rasmus Haugaard <rasmushaugaard@Rasmuss-MacBook-Pro.local>
2017-07-05 13:44:57 +0200

    flightplan key from env

--- flightplan.js
+++ flightplan.js
@@ -3,18 +3,10 @@ var plan = require('flightplan');
 var appName = 'node-app';
 var username = 'deploy';
 var startFile = 'index.js';
-var keyfile = "/home/mneerup/.ssh/enjam_deploy";
+
+var {keyfile} = require('./env');
 
 var tmpDir = appName+'-' + new Date().getTime();
-
-// configuration
-plan.target('staging', [
-  {
-    host: '104.131.93.214',
-    username: username,
-    agent: process.env.SSH_AUTH_SOCK
-  }
-]);
 
 plan.target('production', [
   {
@@ -23,20 +15,10 @@ plan.target('production', [
     privateKey: keyfile,
     agent: process.env.SSH_AUTH_SOCK
   },
-//add in another server if you have more than one
-// {
-//   host: '104.131.93.216',
-//   username: username,
-//   agent: process.env.SSH_AUTH_SOCK
-// }
 ]);
 
 // run commands on localhost
 plan.local(function(local) {
-  // uncomment these if you need to run a build on your machine first
-  // local.log('Run build');
-  // local.exec('gulp build');
-
   local.log('Copy files to remote hosts');
   var filesToCopy = local.exec('git ls-files', {silent: true});
   // rsync files to all the destination's hosts
