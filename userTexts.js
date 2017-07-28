fbda6b3034a1523e46371c3cd32426bbc4d98906
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2017-07-28 17:41:14 +0200

    translate to english

--- userTexts.js
+++ userTexts.js
@@ -6,37 +6,37 @@ function randomEntry(arr){
 
 function slowPatternSubmit(){
   return randomEntry([
-    "Hurtigere næste gang..",
-    "Tiden løb ud..",
-    "zzz..",
+    "Sorry, I fell asleep. Please try again!",
+    "Oops, the time ran out. Please try again!",
+    "Try entering the pattern and pressing the 'coffee' button.",
   ]);
 }
 
 function wrongPattern(){
   return randomEntry([
-    "Forkert mønster..",
-    "Har du vendt mønsteret på hovedet?",
-    "Har du spejlvendt mønsteret?",
-    "Var det det rigtige mønster?",
+    "Oops, wrong pattern. Please try again!",
+    "Sorry, the pattern seems to be wrong. Please try again!",
+    "Woops! You can find the pattern on the coffee machine.",
+    "According to my assistant the pattern was not correct. Please try again.",
   ]);
 }
 
 function dispenseError(){
   return randomEntry([
-    "Kaffen sidder fast i maskinen..",
-    "Noget gik galt, prøv igen..",
-    "Er kaffemaskinen tændt?",
-    "Tandhjulet sidder fast..",
+    "Something went wrong. I'm sorry!",
+    "Hmm.. That didn't work. Try again!",
+    "That didn't work. Is the coffee machine on?",
+    "That didn't work. Is the wheel stuck?",
   ]);
 }
 
 function dispenseSuccess(){
   return randomEntry([
-    "Hav en god dag!",
-    "Vi ses til IoTjam d. 29. sep - 1. okt!",
-    "Pas på - kaffepulveret er varmt!",
-    "Du er sød!",
-    "Kom igen i morgen!",
+    "Have a great day!",
+    "See you the 29th of september!",
+    "Watch out - the instant coffee is hot!",
+    "I like you! Will you be back tomorrow?",
+    "Have a nice day!",
   ]);
 }
 
